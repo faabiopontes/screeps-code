6a47496ba4510a10258a043328c3262be7eb7301
faabiopontes <faabiopontes@gmail.com>
2018-03-17 00:41:37 -0300

    FEA Now Picking Up Dropped Energy - TODO: Only go after it by range, not everyone in the room

--- default/role.upgrader.js
+++ default/role.upgrader.js
@@ -1,25 +1,32 @@
 var roleUpgrader = {
 
     /** @param {Creep} creep **/
-    run: function(creep) {
+    run: function (creep) {
 
-        if(creep.memory.upgrading && creep.carry.energy == 0) {
+        if (creep.memory.upgrading && creep.carry.energy == 0) {
             creep.memory.upgrading = false;
             creep.say('🔄 harvest');
         }
-        if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+        if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
             creep.memory.upgrading = true;
             creep.say('🚧 upgrading');
         }
-        
-        if(!creep.memory.upgrading) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[creep.memory.mineSource]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[creep.memory.mineSource], { visualizePathStyle: { stroke: '#900' } });
+
+        if (!creep.memory.upgrading) {
+            const target = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY);
+            if (target) {
+                if (creep.pickup(target) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+                }
+            } else {
+                var sources = creep.room.find(FIND_SOURCES);
+                if (creep.harvest(sources[creep.memory.mineSource]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[creep.memory.mineSource], { visualizePathStyle: { stroke: '#900' } });
+                }
             }
         }
         else {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: '#900' } });
             }
         }
